6646f9230edeaf38b59aac57843bfc5c25ce8a54
Geoffrey Lessel <geo@geoffreylessel.com>
2021-01-28 13:22:24 -0800

    chore: update .iex.exs with conveniences

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,12 @@
 alias Ex6502.{Computer, CPU, Executor, Memory}
+
+use Bitwise
+
+IEx.configure(
+  inspect: [
+    base: :hex,
+    charlists: :as_lists,
+    limit: :infinity,
+    pretty: true
+  ]
+)
