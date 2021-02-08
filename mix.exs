c7ece6bd01faf4d63636eb96bfeaa1c62697dca1
Geoffrey Lessel <geo@geoffreylessel.com>
2021-02-07 21:23:27 -0800

    chore(mix): set up project for hex publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Ex6502.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/geolessel/ex6502"
+
   def project do
     [
       app: :ex6502,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      description: description(),
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -22,8 +28,33 @@ defmodule Ex6502.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    An emulation of the famous 6502 processor. Focus is on modern versions
+    of the processor (as opposed to the MOS 6502 version).
+    """
+  end
+
+  defp package do
+    [
+      name: :ex6502,
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Geoffrey Lessel"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      name: "Ex6502",
+      source_url: @source_url,
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
